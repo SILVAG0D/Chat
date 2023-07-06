8278bc408b5309b84c3fafa4692b72afceb1c39b
gustavo.liesch <gustavo.liesch@dbm.com.br>
2023-07-06 09:13:45 -0300

    inicio do projeto

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:126473927078:web:d9d0968ef7ecc0c2daf767";
+  static String apiKey = "AIzaSyAJwc3OulrRtCxufGYWx-Ar9huQzqOAk00";
+  static String messagingSenderId = "126473927078";
+  static String projectId = "chatapp-abd10";
+}
